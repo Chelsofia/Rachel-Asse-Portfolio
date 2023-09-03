c3ec5892cee02464c98b89812e23d172dfe852e8
Rachel Asse <86608086+Chelsofia@users.noreply.github.com>
2023-09-03 17:39:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+
+gsap.to(".services", {
+  y:-55,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".services",
+  toggleActions: "restart reverse none none",
+ }
+})
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".about-me img",
+    toggleActions: "play reverse none none"
+  }
+})
+tl.to (".about-me img",{y:-10, duration: 3})
+
+gsap.to(".about-me p", {
+  y: 700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".services",
+    toggleActions: "play reverse none none",
+  },
+});
